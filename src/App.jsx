bcfb8d51f2288a84bb603be7e1608f6a24be5bb1
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-03-28 11:13:46 +0600

    add card picture

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
     <div className="App">
       
       <Header></Header>
-      
+       <Shop></Shop>
     </div>
   )
 }
